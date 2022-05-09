2087a943ae2011ea84228b31ef919fc81f0c8e83
Lukas Gjetting <lukasgjetting@gmail.com>
2022-05-09 13:17:45 +0200

    Update colors and favicon

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -21,16 +21,16 @@ const Contact: NextPage = () => (
           </h2>
           <br />
           <a href="mailto:hi@lukasgjetting.com" className="mb-5 flex items-center">
-            <Image width={24} height={24} src="/icon-envelope.svg" alt="Envelope icon" />
-            <span className="pl-2 text-[#1e73be]">hi@lukasgjetting.com</span>
+            <Image className="opacity-50" width={24} height={24} src="/icon-envelope.svg" alt="Envelope icon" />
+            <span className="pl-2 text-[#7c3aed]">hi@lukasgjetting.com</span>
           </a>
           <a href="https://m.me/lukasgjetting" target="_blank" rel="noreferrer" className="mb-5 flex items-center">
-            <Image width={24} height={24} src="/icon-messenger.svg" alt="Messenger icon" />
-            <span className="pl-2 text-[#1e73be]">Message me on Facebook</span>
+            <Image className="opacity-50" width={24} height={24} src="/icon-messenger.svg" alt="Messenger icon" />
+            <span className="pl-2 text-[#7c3aed]">Message me on Facebook</span>
           </a>
           <a href="https://www.linkedin.com/in/lukasgjetting/" target="_blank" rel="noreferrer" className="mb-3 flex items-center">
-            <Image width={24} height={24} src="/icon-linkedin.svg" alt="LinkedIn icon" />
-            <span className="pl-2 text-[#1e73be]">Connect on LinkedIn</span>
+            <Image className="opacity-50" width={24} height={24} src="/icon-linkedin.svg" alt="LinkedIn icon" />
+            <span className="pl-2 text-[#7c3aed]">Connect on LinkedIn</span>
           </a>
         </div>
       </div>
