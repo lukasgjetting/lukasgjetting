f28497affd74f17c6b72d483774ecd9dff57a522
Lukas Gjetting <lukasgjetting@gmail.com>
2022-06-03 14:42:10 +0200

    Set domain to lukasgjetting.com

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: 'https://lukasgjetting.dk',
+    siteUrl: 'https://lukasgjetting.com',
     generateRobotsTxt: true,
 };
 
